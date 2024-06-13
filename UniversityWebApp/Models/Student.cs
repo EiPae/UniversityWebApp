ff6b7d3505818cb909f4a51008531c3c4767be8f
EiPae <eithaepomyanmar05033@gmail.com>
2024-06-13 23:19:42 +0900

    Upload universityWebApp Project

---
+++ UniversityWebApp/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniversityWebApp.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string? StudentName { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? Coure { get; set; }
+
+    public string? Address { get; set; }
+}
---
+++ UniversityWebApp/Models/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniversityWebApp.Models;
+
+public partial class Teacher
+{
+    public int Id { get; set; }
+
+    public string? TeacherName { get; set; }
+
+    public int? Age { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? Subject { get; set; }
+}
